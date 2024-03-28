34ee031cd380a2643faf9905d6a423b9309d956e
Tan Ho <th@localizedirect.com>
2024-03-28 14:39:09 +0700

    Add JsonProperty annotation on response fields

--- src/main/java/io/github/stefanbratanov/jvm/openai/Images.java
+++ src/main/java/io/github/stefanbratanov/jvm/openai/Images.java
@@ -1,8 +1,9 @@
 package io.github.stefanbratanov.jvm.openai;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 public record Images(long created, List<Image> data) {
 
-  public record Image(String b64Json, String url, String revisedPrompt) {}
+  public record Image(String b64Json, String url, @JsonProperty("revised_prompt") String revisedPrompt) {}
 }
--- src/main/java/io/github/stefanbratanov/jvm/openai/Usage.java
+++ src/main/java/io/github/stefanbratanov/jvm/openai/Usage.java
@@ -1,4 +1,6 @@
 package io.github.stefanbratanov.jvm.openai;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 /** Usage statistics */
-public record Usage(int completionTokens, int promptTokens, int totalTokens) {}
+public record Usage(@JsonProperty("completion_tokens") int completionTokens, @JsonProperty("prompt_tokens") int promptTokens, @JsonProperty("total_tokens") int totalTokens) {}
